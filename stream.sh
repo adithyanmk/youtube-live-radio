b217885d6a9344e050c248f2e552c62b310e287a
adithyanmk <108169604+adithyanmk@users.noreply.github.com>
2022-07-04 13:14:14 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,11 +4,12 @@ set -e
 
 while true
 do
+  KEY="bhga-sfzg-mm33-t5bt-2fkx"
   ffmpeg -loglevel info -y -re \
     -f image2 -loop 1 -i bg.png \
     -f concat -safe 0 -i <((for f in ./mp3/*.mp3; do path="$PWD/$f"; echo "file ${path@Q}"; done) | shuf) \
     -c:v libx264 -preset veryfast -b:v 3000k -maxrate 3000k -bufsize 6000k \
     -framerate 25 -video_size 1280x720 -vf "format=yuv420p" -g 50 -shortest -strict experimental \
     -c:a aac -b:a 128k -ar 44100 \
-    -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+    -f flv rtmp://a.rtmp.youtube.com/live2/$KEY
 done
